a6fab5f401390b85195e6a85933b95d9b66a9f35
ogimly <ogimly@gmail.com>
2022-04-27 01:31:16 +0500

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason( date ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (!date) return 'Unable to determine the time of year!';
+
+  //console.log(date);
+
+  try {date.getTime()}
+  catch {
+    throw new Error('Invalid date!');
+  }
+
+  let arrSeason = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter'];
+  try {
+    let arrSeason = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter'];
+    return arrSeason[date.getMonth()];
+  } catch {
+    throw new Error('Invalid date!');
+  }
+
 }
 
 module.exports = {
   getSeason
 };
+
+
+//console.log(getSeason(new Date(2020, 02, 31)))
+
+// const deeperFakeDate = {
+//   toString() {
+//       return Date.prototype.toString.call(new Date());
+//   },
+//   getMonth() {
+//       return Date.prototype.getMonth.call(new Date());
+//   },
+//   getFullYear() {
+//       return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
+//   },
+//   getDate() {
+//       return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
+//   },
+//   getHours() {
+//       return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
+//   },
+//   getMinutes() {
+//       return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
+//   },
+//   getSeconds() {
+//       return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
+//   },
+//   getMilliseconds() {
+//       return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
+//   },
+//   getDay() {
+//       return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
+//   },
+//   [Symbol.toStringTag]: 'Date'
+// };
+
+// Object.setPrototypeOf(deeperFakeDate, Object.getPrototypeOf(new Date()));
+
+// console.log(getSeason(deeperFakeDate));
