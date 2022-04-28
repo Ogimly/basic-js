3faaa6ad4c4fd9c055fd6b986a22aea1196c1aac
ogimly <ogimly@gmail.com>
2022-04-29 02:23:58 +0500

    feat: hanoi & other

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,11 +14,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi( disksNumber, turnsSpeed ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // Рекурсивно решаем задачу «перенести башню из n−1 диска c 0-го на 1-й штырь». 
+  // Затем переносим самый большой диск на 2-й штырь, и рекурсивно решаем задачу «перенеси башню из n−1 диска с 1-го на 2-й штырь».
+  const moveHanoi = (disksNumber) => {
+
+    if (disksNumber === 0) return 0;
+
+    let turns = 1;
+
+    turns += 2 * moveHanoi(disksNumber-1);
+    
+    return turns;
+  }
+
+  let res = {
+    turns: 0,
+    seconds: 0
+  };
+
+  res.turns = moveHanoi(disksNumber);
+  res.seconds = Math.floor(res.turns / (turnsSpeed/3600));
+
+  return res;
 }
 
 module.exports = {
   calculateHanoi
 };
+
+//console.log(calculateHanoi( 9, 4308 ));
